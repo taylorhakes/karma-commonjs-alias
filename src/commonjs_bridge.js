e1cb3dc18f8665f692cab92135604207df621cc2
taylorhakes <taylor.hakes@gmail.com>
2014-03-11 19:57:25 -0400

    Added the ability to put npm modules in tests

--- src/commonjs_bridge.js
+++ src/commonjs_bridge.js
@@ -3,12 +3,13 @@ var cachedModules = {};
 // load all modules
 for (var modulePath in window.__cjs_module__) {
     require(modulePath, modulePath);
-};
+}
 
 function require(requiringFile, dependency) {
 
-    if (window.__cjs_module__ === undefined) throw new Error("Could not find any modules. Did you remember to set 'preprocessors' in your Karma config?");
-    if (window.__cjs_modules_root__ === undefined) throw new Error("Could not find CommonJS module root path. Please report this issue to the karma-commonjs project.");
+    if (window.__cjs_module__ === undefined) {
+        throw new Error("Could not find any modules. Did you remember to set 'preprocessors' in your Karma config?");
+    }
 
     if (!window.__cjs_module__[dependency]) {
         dependency = normalizePath(requiringFile, dependency);
@@ -16,7 +17,10 @@ function require(requiringFile, dependency) {
 
     // find module
     var moduleFn = window.__cjs_module__[dependency];
-    if (moduleFn === undefined) throw new Error("Could not find module '" + dependency + "' from '" + requiringFile + "'");
+    if (moduleFn === undefined) {
+        throw new Error("Could not find module '" + dependency + "' from '" +
+            requiringFile + "'");
+    }
 
     // run the module (if necessary)
     var module = cachedModules[dependency];
@@ -32,31 +36,31 @@ function requireFn(basepath) {
     return function(dependency) {
         return require(basepath, dependency);
     };
-};
+}
 
-function normalizePath(basePath, relativePath, modulesRoot) {
-
+function normalizePath(basePath, relativePath) {
     if (isFullPath(relativePath)) return relativePath;
-    if (isNpmModulePath(relativePath)) basePath = modulesRoot;
     if (!isFullPath(basePath)) throw new Error("basePath should be full path, but was [" + basePath + "]");
-
-    var relativePathLeadingChar = relativePath.charAt(0);
 
     var baseComponents = basePath.split("/");
     var relativeComponents = relativePath.split("/");
     var nextComponent;
-    
-    if (!isNpmModulePath(relativePath)) {
-        baseComponents.pop();     // remove file portion of basePath before starting
-    }
+
+    baseComponents.pop();
     while (relativeComponents.length > 0) {
         nextComponent = relativeComponents.shift();
 
-        if (nextComponent === ".") continue;
-        else if (nextComponent === "..") baseComponents.pop();
-        else baseComponents.push(nextComponent);
+        if (nextComponent === ".") {
+            continue;
+        }
+        else if (nextComponent === "..") {
+            baseComponents.pop();
+        }
+        else {
+            baseComponents.push(nextComponent);
+        }
     }
-    
+
     var normalizedPath = baseComponents.join("/");
 
     if (normalizedPath.substr(normalizedPath.length - 3) !== ".js") {
@@ -65,15 +69,10 @@ function normalizePath(basePath, relativePath, modulesRoot) {
 
     return normalizedPath;
 
-
     function isFullPath(path) {
         var unixFullPath = (path.charAt(0) === "/");
         var windowsFullPath = (path.indexOf(":") !== -1);
 
         return unixFullPath || windowsFullPath;
-    }
-
-    function isNpmModulePath(path) {
-      return path.charAt(0) !== ".";
     }
 }
